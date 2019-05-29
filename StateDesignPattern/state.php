44b96314bac9257b1b8e3229ad90e6b25a8bf7ca
unknown <davidsolomon971@gmail.com>
2019-05-29 15:10:56 +0300

    dave commit

---
+++ StateDesignPattern/state.php
@@ -0,0 +1,30 @@
+<?php
+interface WritingState
+{
+    public function write(string $words);
+}
+
+class UpperCase implements WritingState
+{
+    public function write(string $words)
+    {
+        echo strtoupper($words);
+    }
+}
+
+class LowerCase implements WritingState
+{
+    public function write(string $words)
+    {
+        echo strtolower($words);
+    }
+}
+
+class DefaultText implements WritingState
+{
+    public function write(string $words)
+    {
+        echo $words;
+    }
+}
+?>
---
+++ StateDesignPattern/test.php
@@ -0,0 +1,17 @@
+<?php
+include("state.php");
+include("textEditor.php");
+$editor = new TextEditor(new DefaultText());
+
+$editor->type('First line');
+
+$editor->setState(new UpperCase());
+
+$editor->type('Second line');
+$editor->type('Third line');
+
+$editor->setState(new LowerCase());
+
+$editor->type('Fourth line');
+$editor->type('Fifth line');
+?>
